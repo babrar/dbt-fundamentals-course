72f4086ab554da5a714fd54becfa4c2278678838
Banin Abrar <babrar@uwaterloo.ca>
2023-12-05 23:11:27 +0000

    Added tests and documentation

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,8 +3,8 @@ select
     orderid as order_id,
     paymentmethod as method,
     status,
-    amount,
+    -- amount is stored in cents, convert to dollars
+    amount / 100 as amount,
     created as created_at
 from 
     {{ source('stripe', 'payment') }}
-    
